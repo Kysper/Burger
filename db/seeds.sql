5f21e370275c3780ed3ad6f5060755a0f8e633e6
Kysper <kysper50@gmail.com>
2018-11-17 19:53:31 -0600

    Finished Product

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon Cheddar w/Pretzel Bun",FALSE);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Baconator",FALSE);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Whiskey Smoked Burger",FALSE);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Whopper",FALSE);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Teriyaki Burger",FALSE);
---
+++ models/burger.js
@@ -0,0 +1,25 @@
+var orm = require('../config/orm.js');
+
+//Create code that will call the ORM functions using burger specific input for the ORM
+
+
+var burger = {
+   sAll: function(cb){
+      orm.selectAll("burgers", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    create: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    update: function(objColVals, condition, cb) {
+      orm.updateOne("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    },
+  };
+
+module.exports = burger;
